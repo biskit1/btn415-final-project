eec2ee7ed4ba4075c8b7623cf5f0a87692f19346
Stuart Crust <scrust@myseneca.ca>
2018-03-31 16:30:53 -0400

    fix(pktdef): reorder bitfields
    Reorders bitfields in `struct Header` to match the spec.

--- src/PktDef.h
+++ src/PktDef.h
@@ -27,11 +27,11 @@ struct MotorBody {
 class PktDef {
 	struct Header {
 		unsigned int PktCount;
-		unsigned char Sleep : 1;
-		unsigned char Status : 1;
 		unsigned char Drive : 1;
-		unsigned char Claw : 1;
+		unsigned char Status : 1;
+		unsigned char Sleep : 1;
 		unsigned char Arm : 1;
+		unsigned char Claw : 1;
 		unsigned char Ack : 1;
 		unsigned char Padding : 2;
 		unsigned char Length;
