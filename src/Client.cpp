9629cc2024e933992ea269ed73eb248c175c92e7
UltimaBGD <bdennis2@myseneca.ca>
2018-04-15 12:04:16 -0400

    12 to Constant

--- src/Client.cpp
+++ src/Client.cpp
@@ -8,8 +8,10 @@
 #include "PktDef.h"
 #include "Telemetry.h"
 
+#define TELPKTSIZE 12
+
 bool ExeComplete;
-std::string makeHex(char * pkt);
+std::string makeHex(char * pkt, int size);
 void telemetryThread(std::string Ip, int TelPort);
 
 //order of arguements: IP, Com, Tel 
@@ -38,12 +40,12 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-std::string makeHex(char * pkt) {
+std::string makeHex(char * pkt, int size) {
 	std::string hexString;
 	std::stringstream stream;
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < size; i++) {
 		stream << std::hex << (int)pkt[i];
-		if (i != 11) {
+		if (i != size - 1) {
 			stream << ", ";
 		}
 	}
@@ -55,7 +57,7 @@ std::string makeHex(char * pkt) {
 void telemetryThread(std::string Ip, int TelPort) {
 	//all Telemetry Packets are of size 12
 	std::ofstream ofs("Telemetry.txt", std::ofstream::out);
-	MySocket socket(SocketType::CLIENT, Ip, TelPort, ConnectionType::TCP, 12);
+	MySocket socket(SocketType::CLIENT, Ip, TelPort, ConnectionType::TCP, TELPKTSIZE);
 	if (socket.ConnectTCP()) {
 		char rx_buffer[255];
 		int size;
@@ -64,7 +66,7 @@ void telemetryThread(std::string Ip, int TelPort) {
 			if (size > 0) {
 				PktDef TelPkt(rx_buffer);
 				if (size != TelPkt.GetLength()) {
-					std::cout << "ERROR: Packet recieved is not of the same size!" << std::endl;
+					std::cout << "ERROR: Packet recieved is not of the expected size!" << std::endl;
 				}
 				else {
 					if (!TelPkt.CheckCRC(rx_buffer, TelPkt.GetLength())) {
@@ -75,8 +77,7 @@ void telemetryThread(std::string Ip, int TelPort) {
 							std::cout << "ERROR: Command is not Status!";
 						}
 						else {
-							Telemetry tel(TelPkt.GetBodyData(), TelPkt.GetPktCount(), makeHex(rx_buffer));
-							//std::cout << tel.toString(MULTI);
+							Telemetry tel(TelPkt.GetBodyData(), TelPkt.GetPktCount(), makeHex(rx_buffer, TelPkt.GetLength()));
 							ofs << tel.toString(MULTI);
 						}
 					}
